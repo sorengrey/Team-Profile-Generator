75ba565ce3ccf389de76ff86e7f97624e55aaf64
Heather Smith <halexsmith86@gmail.com>
2021-02-23 16:39:52 -0500

    Added prompts for engineer and intern

--- index.js
+++ index.js
@@ -85,8 +85,67 @@ function promptUser() {
             name: 'newmember',
             choices: ['Engineer', 'Intern', 'No thanks'],
         },
-        // need to figure out how to make the choice of engineer or intern show up as a variable - read the inquirer documentation!
+        // testing -- need to figure out how to make the choice of engineer or intern
     ]).then(response => {
+        if (response.newmember === 'Engineer'){
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'What is the engineer\'s name?',
+                    name: 'manager',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the team engineer\'s employee ID number?',
+                    name: 'id'
+                },
+                {
+                    type: 'input',
+                    message: 'What is the team engineer\'s email?',
+                    name: 'email',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the team engineer\'s phone number?',
+                    name: 'usage',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the engineer\'s Github username?',
+                    name: 'github',
+                },
+            ])
+        } if(response.newmember === 'Intern') {
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'What is the intern\'s name?',
+                    name: 'manager',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the intern\'s employee ID number?',
+                    name: 'id'
+                },
+                {
+                    type: 'input',
+                    message: 'What is the intern\'s email?',
+                    name: 'email',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the intern\'s phone number?',
+                    name: 'usage',
+                },
+                {
+                    type: 'input',
+                    message: 'What school did the intern go to?',
+                    name: 'school',
+                },
+            ])
+        } else {
+            // not sure what to put here yet
+        }
     })
 }
 
