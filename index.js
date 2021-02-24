13374de8ca37650b5e86605485ae8a1993d21e59
Heather Smith <halexsmith86@gmail.com>
2021-02-24 16:38:34 -0500

    Moved engineer and intern prompts to class files

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const inquirer = require("inquirer");
 const jest = require("jest");
 
 // imported classes
-import Employee from './lib/employee.js';
-import Intern from './lib/intern.js';
-import Engineer from './lib/engineer.js';
-import Manager from './lib/manager.js';
+const Employee = require('./lib/employee.js');
+const Intern = require('./lib/intern.js');
+const Engineer = require('./lib/engineer.js');
+const Manager = require('./lib/manager.js');
 
 
 // inquirer prompts
@@ -30,80 +30,23 @@ function promptUser() {
         {
             type: 'input',
             message: 'What is the team manager\'s phone number?',
-            name: 'usage',
+            name: 'phone',
         },
         {
             type: 'list',
             message: 'Would you like to add another team member?',
             name: 'newmember',
             choices: ['Engineer', 'Intern', 'No thanks'],
-        },
-    ]).then(response => {
-        if (response.newmember === 'Engineer'){
-            return inquirer.prompt([
-                {
-                    type: 'input',
-                    message: 'What is the engineer\'s name?',
-                    name: 'manager',
-                },
-                {
-                    type: 'input',
-                    message: 'What is the team engineer\'s employee ID number?',
-                    name: 'id'
-                },
-                {
-                    type: 'input',
-                    message: 'What is the team engineer\'s email?',
-                    name: 'email',
-                },
-                {
-                    type: 'input',
-                    message: 'What is the team engineer\'s phone number?',
-                    name: 'usage',
-                },
-                {
-                    type: 'input',
-                    message: 'What is the engineer\'s Github username?',
-                    name: 'github',
-                },
-            ])
-        } if(response.newmember === 'Intern') {
-            return inquirer.prompt([
-                {
-                    type: 'input',
-                    message: 'What is the intern\'s name?',
-                    name: 'manager',
-                },
-                {
-                    type: 'input',
-                    message: 'What is the intern\'s employee ID number?',
-                    name: 'id'
-                },
-                {
-                    type: 'input',
-                    message: 'What is the intern\'s email?',
-                    name: 'email',
-                },
-                {
-                    type: 'input',
-                    message: 'What is the intern\'s phone number?',
-                    name: 'usage',
-                },
-                {
-                    type: 'input',
-                    message: 'What school did the intern go to?',
-                    name: 'school',
-                },
-            ])
-        } else {
-            // not sure what to put here yet
         }
-    })
-}
+    ])}
 
 // testing -- 
- //const Ryan = new Intern();
- //console.log(Ryan);
+//  const Ryan = new Intern();
+//  const Darrell = new Engineer();
+//  const Michael = new Manager();
+// console.log(Ryan);
 
  // launches the prompts
 promptUser();
+
+// module.exports = promptUser();
